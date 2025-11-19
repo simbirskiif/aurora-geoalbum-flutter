1e6633fbffaf42ba2e712bac2cd19b61f8735420
Simbirskiif <simbirskiif@gmail.com>
2025-11-19 18:46:24 +0400

    19.11

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:geo_album/image_store.dart';
